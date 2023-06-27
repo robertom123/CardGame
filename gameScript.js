09b59bb278843e9c0fc82e7878198e5da8c70e03
robertom123 <martinez365r@gmail.com>
2023-06-26 17:15:05 -0700

    Worked on play-area

--- gameScript.js
+++ gameScript.js
@@ -17,3 +17,16 @@ let memGauge = () => {
 }
 
 memGauge()
+
+let cards = document.querySelector('.hand-cards');
+let w = cards.offsetWidth;
+let totalarc = 270;
+let numcards = 7;
+let angles = Array(numcards).fill('').map((a, i) => (totalarc / numcards * (i + 1)) - (totalarc/2 + (totalarc / numcards) / 2));
+let margins = angles.map((a, i) => w / numcards * (i + 1));
+
+angles.forEach((a, i) => {
+  let s = `transform:rotate(${angles[i]}deg);margin-left:${margins[i]}px;`
+  let c = `<div class='card' style='${s}'></div>`;
+  cards.innerHTML += c;
+})
